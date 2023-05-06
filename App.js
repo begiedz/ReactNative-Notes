0e9dca0dae276dac63c1a2403037a050b5d497fd
begiedz <dariusz.begiedza@gmail.com>
2023-05-06 11:01:11 +0200

    add basic structure

--- App.js
+++ App.js
@@ -4,8 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Tytuł taska</Text>
+        <View style={styles.items}></View>
+      </View>
     </View>
   );
 }
@@ -13,8 +15,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    flexWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  }
 });
